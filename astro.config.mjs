586e4c43976900768f42ee305ed29b9c2bd835b0
Kyle Scudder <kyle@kylescudder.co.uk>
2024-12-11 10:32:50 +0000

    feat(seo): added seo metadata

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,10 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: process.env.CI
+    ? "https://jaiscudderplumbing.co.uk"
+    : "http://localhost:4321",
+
   integrations: [tailwind(), react()],
   output: "server",
   adapter: vercel(),
